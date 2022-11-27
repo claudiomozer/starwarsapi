1d0c8de75f3873c55be87675ce19f38006f0e1f2
Cláudio André Mozer <claudinhoandre@gmail.com>
2022-11-27 20:34:09 -0300

    feat: ensure Http helper return a valid response on success

---
+++ src/infra/repositories/http/http.helper.go
@@ -0,0 +1,19 @@
+package httpclient
+
+import (
+	"io"
+	"net/http"
+)
+
+func Get(url string) (status int, body []byte, err error) {
+
+	response, err := http.Get(url)
+
+	if err == nil {
+		defer response.Body.Close()
+		status = response.StatusCode
+		body, err = io.ReadAll(response.Body)
+	}
+
+	return
+}
