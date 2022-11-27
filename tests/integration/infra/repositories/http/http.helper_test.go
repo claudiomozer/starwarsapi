fc3c640c5ed475c433d1220c4beae3e9a663fab9
Cláudio André Mozer <claudinhoandre@gmail.com>
2022-11-27 20:49:59 -0300

    test: ensure HttpHelper returns an error when an invalid URL is given

--- tests/integration/infra/repositories/http/http.helper_test.go
+++ tests/integration/infra/repositories/http/http.helper_test.go
@@ -21,3 +21,19 @@ func TestShouldReturnAResponseOnSuccess(t *testing.T) {
 		t.Error("Should return a not empty body on success")
 	}
 }
+
+func TestShouldReturnErrorIfInvalidUrlIsProvided(t *testing.T) {
+	status, body, err := httpclient.Get("https://invalidurl.get/invalidresource")
+
+	if err == nil {
+		t.Error("Should return error when an invalid URL is given")
+	}
+
+	if status != 0 {
+		t.Error("Should not return a valid status on error")
+	}
+
+	if len(body) != 0 {
+		t.Error("Should return a empty body on error")
+	}
+}
