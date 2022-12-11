b3bb000497e02e83ff1be2d398bf7a804b828d13
Cláudio André Mozer <claudinhoandre@gmail.com>
2022-12-11 19:55:46 -0300

    feat: ensure FilmRepository returns an id on success

--- src/infra/repositories/db/mongodb/film.repository.go
+++ src/infra/repositories/db/mongodb/film.repository.go
@@ -3,8 +3,11 @@ package mongodb
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	domaindto "github.com/claudiomozer/starwarsapi/src/domain/dtos"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type FilmRepository struct {
@@ -21,7 +24,16 @@ func (repo *FilmRepository) Create(filmDTO *domaindto.FilmDTO) (id string, err e
 	if repo.isConnectionInvalid() {
 		return "", errors.New("Erro ao criar Film na base de dados. Nenhuma conexão com banco de dados estabelecida")
 	}
-	return "", nil
+
+	collection := Helper.GetCollection(repo.collection)
+	result, err := collection.InsertOne(context.TODO(), repo.getBson(filmDTO))
+
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("Erro ao inserir Film na base de dados: %v\n", err))
+	}
+
+	objectId := result.InsertedID.(primitive.ObjectID)
+	return objectId.String(), err
 }
 
 func (repo *FilmRepository) isConnectionInvalid() bool {
@@ -34,4 +46,13 @@ func (repo *FilmRepository) isConnectionInvalid() bool {
 	}
 
 	return false
+}
+
+func (repo *FilmRepository) getBson(filmDTO *domaindto.FilmDTO) interface{} {
+	return bson.D{
+		primitive.E{Key: "title", Value: filmDTO.Title},
+		primitive.E{Key: "director", Value: filmDTO.Director},
+		primitive.E{Key: "release_date", Value: filmDTO.ReleaseDate},
+		primitive.E{Key: "url", Value: filmDTO.Url},
+	}
 }
