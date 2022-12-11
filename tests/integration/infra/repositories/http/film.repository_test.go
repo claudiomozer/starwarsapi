589f36b385efac10d4c6dd07f4d02cb4faba5fb5
Cláudio André Mozer <claudinhoandre@gmail.com>
2022-12-11 11:09:36 -0300

    test: ensure FilmRepository returns a valid FilmDTO on success

--- tests/integration/infra/repositories/http/film.repository_test.go
+++ tests/integration/infra/repositories/http/film.repository_test.go
@@ -14,6 +14,7 @@ func FuzzShouldReturnAnErrorIfAnInvalidURLIsGiven(f *testing.F) {
 	f.Add("https://swapi.dev/api/films/5e3e3")
 	f.Add("https://swapi.dev/api/films/53^")
 	f.Add("https://swapi.dev/api/films/a53")
+	f.Add("http://swapi.dev/api/films/5/4")
 
 	f.Fuzz(func(t *testing.T, invalidUrl string) {
 		sut := httprepo.NewFilmRepository()
@@ -35,5 +36,36 @@ func TestShouldReturnEmptyIfGivenUrlDoesNotExist(t *testing.T) {
 
 	if film != nil {
 		t.Error("Film must be null")
+	}
+}
+
+func TestShouldReturnAValidFilmDtoOnSuccess(t *testing.T) {
+	sut := httprepo.NewFilmRepository()
+	validUrl := "https://swapi.dev/api/films/1/"
+	film, err := sut.Load(validUrl)
+
+	if err != nil {
+		t.Error("Should not return an error on success")
+	}
+
+	if film == nil {
+		t.Error("Film must not be null on success")
+		return
+	}
+
+	if film.Url != validUrl {
+		t.Error("Must return a valid film on success")
+	}
+
+	if film.Title == "" {
+		t.Error("Must return a film with a valid title")
+	}
+
+	if film.ReleaseDate == "" {
+		t.Error("Must return a film with a valid release date")
+	}
+
+	if film.Director == "" {
+		t.Error("Must return a film with a valid director")
 	}
 }
