63aacc303194a0e379509f356b0fcb983362ba24
Cláudio André Mozer <claudinhoandre@gmail.com>
2022-12-18 21:27:56 -0300

    test: ensure FilmRepository returns an error if invalid URL is given

--- tests/integration/infra/repositories/db/mongodb/film.repository_test.go
+++ tests/integration/infra/repositories/db/mongodb/film.repository_test.go
@@ -58,3 +58,23 @@ func TestShouldReturnsAnIdOnSuccess(t *testing.T) {
 		return
 	}
 }
+
+func FuzzShouldReturnAnErrorIfAnInvalidURLIsGiven(f *testing.F) {
+	f.Add("https://invalidurl.com.br/invalid/route")
+	f.Add("https://swapi.dev/api/films/e")
+	f.Add("https://swapi.dev/api/films/#")
+	f.Add("http://swapi.dev/api/films/5")
+	f.Add("https://swapi.dev/api/films/5e3e3")
+	f.Add("https://swapi.dev/api/films/53^")
+	f.Add("https://swapi.dev/api/films/a53")
+	f.Add("http://swapi.dev/api/films/5/4")
+
+	f.Fuzz(func(t *testing.T, invalidUrl string) {
+		sut := mongodb.NewFilmRepository()
+		_, err := sut.GetByUrl(invalidUrl)
+
+		if err == nil {
+			t.Error("Should return an error when an invalid URL is given")
+		}
+	})
+}
