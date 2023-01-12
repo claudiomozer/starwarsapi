b80106ef14c2997374a3f2ae207ec9499125c809
Cláudio André Mozer <claudinhoandre@gmail.com>
2023-01-12 10:59:06 -0300

    test: ensure GetPlanetIdByUrl use case returns empty if repository returns empty

--- tests/unit/data/usecases/get_planet_id_by_url.usecase_test.go
+++ tests/unit/data/usecases/get_planet_id_by_url.usecase_test.go
@@ -54,3 +54,18 @@ func TestShouldReturnAnErrorIfGetPlanetIdByUrlRepositoryReturnsError(t *testing.
 		t.Error("Should return an error if GetPlanetIdByUrlRepository returns error")
 	}
 }
+
+func TestShouldReturnAnEmptyIfGetPlanetIdByUrlRepositoryReturnsEmpty(t *testing.T) {
+	sutParams := MakeGetPlanetIdByUrlUseCaseSutParams()
+	sut := sutParams.sut
+	sutParams.getPlanetIdByUrlRepository.ReturnsEmpty = true
+	id, err := sut.GetByUrl("url.test.com")
+
+	if id != "" {
+		t.Error("Should not return an valid id if empty is returned")
+	}
+
+	if err != nil {
+		t.Error("Should not return an error empty is returned")
+	}
+}
