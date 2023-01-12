07faa2879c1e41d28350a4daf976a2a1aacb00d1
Cláudio André Mozer <claudinhoandre@gmail.com>
2023-01-12 09:16:52 -0300

    test: ensure PlanetRepository returns an valid id on success

--- tests/integration/infra/repositories/db/mongodb/planet.repository_test.go
+++ tests/integration/infra/repositories/db/mongodb/planet.repository_test.go
@@ -5,6 +5,7 @@ import (
 
 	domaindto "github.com/claudiomozer/starwarsapi/src/domain/dtos"
 	"github.com/claudiomozer/starwarsapi/src/infra/repositories/db/mongodb"
+	domainmocks "github.com/claudiomozer/starwarsapi/tests/unit/domain/mocks"
 )
 
 func TestShouldReturnAnErrorIfThereIsNoConnetion(t *testing.T) {
@@ -18,5 +19,35 @@ func TestShouldReturnAnErrorIfThereIsNoConnetion(t *testing.T) {
 
 	if err == nil {
 		t.Error("Must return an error if there is no connection")
+	}
+}
+
+func TestShouldCreateAPlanetOnSuccess(t *testing.T) {
+	if err := ConnectWithMongoServer(); err != nil {
+		t.Errorf("Error while connecting to the database:%v\n", err)
+		return
+	}
+
+	filmRepo := mongodb.NewFilmRepository()
+	filmMocked := domainmocks.MockANewHopeFilmDTO()
+
+	id, err := filmRepo.Create(filmMocked)
+
+	if err != nil || id == "" {
+		t.Error("Could not create a planet without films")
+		return
+	}
+
+	planetMocked := domainmocks.MockPlanetDbDTO()
+	planetMocked.Films = []string{id}
+	sut := mongodb.NewPlanetRepository()
+	id, err = sut.Create(planetMocked)
+
+	if id == "" {
+		t.Error("Must return a valid id on success")
+	}
+
+	if err != nil {
+		t.Error("May not return an error on success")
 	}
 }
