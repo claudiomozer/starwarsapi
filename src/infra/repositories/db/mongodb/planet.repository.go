6a58100c8dbefac82488b28a235e842d78c43b50
Cláudio André Mozer <claudinhoandre@gmail.com>
2023-01-12 09:16:43 -0300

    feat: ensure PlanetRepository returns an valid id on success

--- src/infra/repositories/db/mongodb/planet.repository.go
+++ src/infra/repositories/db/mongodb/planet.repository.go
@@ -1,9 +1,13 @@
 package mongodb
 
 import (
+	"context"
 	"errors"
+	"fmt"
 
 	domaindto "github.com/claudiomozer/starwarsapi/src/domain/dtos"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type PlanetRepository struct {
@@ -16,11 +20,29 @@ func NewPlanetRepository() *PlanetRepository {
 	}
 }
 
-func (repository *PlanetRepository) Create(planetDto *domaindto.PlanetDTO) (string, error) {
+func (repo *PlanetRepository) Create(planetDto *domaindto.PlanetDTO) (string, error) {
 
 	if Helper == nil || (Helper != nil && Helper.IsConnectionInvalid()) {
 		return "", errors.New("Erro ao criar planeta na base de dados. Nenhuma conexão com banco de dados estabelecida")
 	}
 
-	return "", nil
+	collection := Helper.GetCollection(repo.collection)
+	result, err := collection.InsertOne(context.TODO(), repo.getBson(planetDto))
+
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("Erro ao inserir Film na base de dados: %v\n", err))
+	}
+
+	objectId := result.InsertedID.(primitive.ObjectID)
+	return objectId.String(), err
+}
+
+func (repo *PlanetRepository) getBson(planetDTO *domaindto.PlanetDTO) interface{} {
+	return bson.D{
+		primitive.E{Key: "name", Value: planetDTO.Name},
+		primitive.E{Key: "climate", Value: planetDTO.Climate},
+		primitive.E{Key: "terrain", Value: planetDTO.Terrain},
+		primitive.E{Key: "films", Value: planetDTO.Films},
+		primitive.E{Key: "url", Value: planetDTO.Url},
+	}
 }
